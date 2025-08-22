cebdbff9e4549c57be55f89440d712b7be4c45fb
Avagatech <avagatech@gmail.com>
2025-08-22 23:55:27 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;6ENWUThK#1z1bACyK4ArpxBHItkSVuo_1bImQ6pbjPZ6-Lv8eiI4"
+  OWNER_NUMBER: "25678847836", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
